c174a94b9f750d8d49a64a607b3aa391d85f751a
Hassaan Mustafavi <hassaanmustafavi12@gmail.com>
2025-10-30 23:18:28 +0500

    ---

--- src/test.py
+++ src/test.py
@@ -8,9 +8,11 @@ if __name__ == "__main__":
         "https://www.auction.com/details/3850-sw-124th-ave-miramar-fl-1868311",
         "https://www.auction.com/details/3209-nw-2nd-pl-cape-coral-fl-1873663",
         "https://www.auction.com/details/6989-calle-del-paz-w-boca-raton-fl-1842251",
+        "https://www.auction.com/details/3209-nw-2nd-pl-cape-coral-fl-1873663",
+        "https://www.auction.com/details/6989-calle-del-paz-w-boca-raton-fl-1842251",
         "https://www.auction.com/details/1753-open-field-loop-brandon-fl-1881948",
     ]
-    test_insert_links(zone="EAST", state="test4", links=test_links)
+    test_insert_links(zone="EAST", state="test5", links=test_links)
 
     #test_prev_update_run(zone="Test", preview_only=True)
 
