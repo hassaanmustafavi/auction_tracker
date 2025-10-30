82ce9776b640a033a6efa199cf12af9869f9c39b
Hassaan Mustafavi <hassaanmustafavi12@gmail.com>
2025-10-30 22:07:06 +0500

    --

--- src/main.py
+++ src/main.py
@@ -932,7 +932,7 @@ def scrape_row_with_driver(
 ):
     try:
         driver.get(link)
-        time.sleep(WAIT_AFTER_EACH_DETAIL_PAGE)
+        #time.sleep(WAIT_AFTER_EACH_DETAIL_PAGE)
         #driver.execute_script("window.focus();")
         WebDriverWait(driver, WAIT_TIME).until(EC.presence_of_element_located((By.CSS_SELECTOR, "[data-elm-id='auction-detail-box-status']")))
         #driver.execute_script("arguments[0].scrollIntoView();", driver.find_element(By.CSS_SELECTOR, "[data-elm-id='add-to-calendar_trigger']"))
