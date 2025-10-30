d7745b815cd7d14facbda0bbcd4cf9cf2affe5ca
Hassaan Mustafavi <hassaanmustafavi12@gmail.com>
2025-10-30 22:48:56 +0500

    -----

--- src/test.py
+++ src/test.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
 
     test_links = [
         "https://www.auction.com/details/17029-peaceful-valley-dr-wimauma-fl-1882044",
-        #"https://www.auction.com/details/7160-davenport-ln-spring-hill-fl-1888836",
-        #"https://www.auction.com/details/3850-sw-124th-ave-miramar-fl-1868311",
-        #"https://www.auction.com/details/3209-nw-2nd-pl-cape-coral-fl-1873663",
-        #"https://www.auction.com/details/6989-calle-del-paz-w-boca-raton-fl-1842251",
+        "https://www.auction.com/details/7160-davenport-ln-spring-hill-fl-1888836",
+        "https://www.auction.com/details/3850-sw-124th-ave-miramar-fl-1868311",
+        "https://www.auction.com/details/3209-nw-2nd-pl-cape-coral-fl-1873663",
+        "https://www.auction.com/details/6989-calle-del-paz-w-boca-raton-fl-1842251",
         "https://www.auction.com/details/1753-open-field-loop-brandon-fl-1881948",
     ]
-    test_insert_links(zone="EAST", state="test2", links=test_links)
+    test_insert_links(zone="EAST", state="test3", links=test_links)
 
     #test_prev_update_run(zone="Test", preview_only=True)
 
